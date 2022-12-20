54fc06355367b08ff93d9dbbff705ab740cdd729
dmitry <dmitryadygezalov@gmail.com>
2022-12-20 19:59:04 +0300

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "User №#{rand(1000)}" if self.name.blank?
+  end
+
+end
