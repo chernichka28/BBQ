49f904d3fdde370f6d2cb2d61f255e193a130540
dmitry <dmitryadygezalov@gmail.com>
2022-12-21 10:51:50 +0300

    Add user features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def user_avatar(user)
+    asset_path("default_avatar.png")
+  end
+
+  def flash_class(level)
+    case level
+        when "notice" then "alert alert-success"
+        when "success" then "alert alert-success"
+        when "error" then "alert alert-danger"
+        when "alert" then "alert alert-danger"
+    end
+  end
 end
