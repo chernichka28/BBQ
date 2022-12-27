d8112b8a3bc766dcff9de87d646a3b11492b291d
dmitry <dmitryadygezalov@gmail.com>
2022-12-27 13:18:10 +0300

    fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :trackable
   has_many :events, dependent: :destroy
-  has_many :comments
-  has_many :subscriptions
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, length: {maximum: 35}
 
