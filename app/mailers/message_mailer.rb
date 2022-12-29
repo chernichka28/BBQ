cfac64fcc6de9c75b9c32308cedbbe5719467809
dmitry <dmitryadygezalov@gmail.com>
2022-12-29 09:41:33 +0300

    devise locale edits

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,13 +5,13 @@ class MessageMailer < ApplicationMailer
     @photo_url = root_url.chop + photo.photo.url
     @url = root_url
 
-    mail to: email, subject: "#{I18n.t("message_mailer.new_photo.subject")}#{@event.title}"
+    mail to: email, subject: default_i18n_subject(title: @event.title)
   end
 
   def new_comment(comment:, email:)
     @comment = comment
     @event = comment.event
 
-    mail to: email, subject: "#{I18n.t("message_mailer.new_comment.subject")}#{@event.title}"
+    mail to: email, subject: default_i18n_subject(title: @event.title)
   end
 end
