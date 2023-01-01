9e7bf539dc123e7a813cd3b06a7a9016866506ae
dmitry <dmitryadygezalov@gmail.com>
2023-01-01 15:20:46 +0300

    fix bug with photo mailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@ class MessageMailer < ApplicationMailer
   def new_photo(email:, photo:)
     @event = photo.event
     @name = photo.user.name
-    @photo_url = photo.photo.url
+    @photo_url = root_url.chop + photo.photo.url
     @event_url = event_url(@event)
 
     mail to: email, subject: default_i18n_subject(title: @event.title)
