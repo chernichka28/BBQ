7e9632e869053cf80e88114d910655ac5e0c17fe
dmitry <dmitryadygezalov@gmail.com>
2023-01-01 15:13:14 +0300

    mailer edits

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,8 +2,8 @@ class MessageMailer < ApplicationMailer
   def new_photo(email:, photo:)
     @event = photo.event
     @name = photo.user.name
-    @photo_url = root_url.chop + photo.photo.url
-    @url = root_url
+    @photo_url = photo.photo.url
+    @event_url = event_url(@event)
 
     mail to: email, subject: default_i18n_subject(title: @event.title)
   end
