78a3de32e3e4b0f320a88298d6f2f9291141d612
dmitry <dmitryadygezalov@gmail.com>
2023-02-03 07:31:19 +0300

    fix policies

--- app/policies/photo_policy.rb
+++ app/policies/photo_policy.rb
@@ -2,8 +2,8 @@ class PhotoPolicy < ApplicationPolicy
 
   def destroy?
     user.present? && (
-      record.user == current_user ||
-      (record.try(:event).present? && record.event.user == current_user)
+      record.user == user ||
+      (record.try(:event).present? && record.event.user == user)
     )
   end
 end
--- app/policies/subscription_policy.rb
+++ app/policies/subscription_policy.rb
@@ -2,8 +2,8 @@ class SubscriptionPolicy < ApplicationPolicy
 
   def destroy?
     user.present? && (
-      record.user == current_user ||
-      (record.try(:event).present? && record.event.user == current_user)
+      record.user == user ||
+      (record.try(:event).present? && record.event.user == user)
     )
   end
 end
