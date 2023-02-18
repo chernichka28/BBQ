9da0dbbc728d35e981c7a332a7435b38677a5fa5
dmitry <dmitryadygezalov@gmail.com>
2023-02-18 11:33:45 +0300

    fix create policies

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,9 +1,5 @@
 class CommentPolicy < ApplicationPolicy
 
-  def create?
-    user.present?
-  end
-
   def destroy?
     can_user_destroy?
   end
--- app/policies/subscription_policy.rb
+++ app/policies/subscription_policy.rb
@@ -1,9 +1,5 @@
 class SubscriptionPolicy < ApplicationPolicy
 
-  def create?
-    user.present?
-  end
-
   def destroy?
     can_user_destroy?
   end
